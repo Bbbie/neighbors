1e8dc0d45cf06b0afada31d210b9e30bb02c6d8e
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-03-01 16:15:16 +0100

    community model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_142341) do
+ActiveRecord::Schema.define(version: 2021_03_01_151131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "communities", force: :cascade do |t|
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

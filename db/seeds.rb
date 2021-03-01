7c24439114957f9775255cffeb5056cf6b37b797
Debbie <sepp.der.bauer@alpenjodel.de>
2021-03-01 18:43:11 +0100

    add icons and illustrations in assets/images

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 CATEGORIES = ['action', 'request', 'info', 'offer', 'question']
+avatars = ["images/empty-avatar_1.svg", "images/empty-avatar_2.svg", "images/empty-avatar_3.svg", "images/empty-avatar_4.svg"]
 
+puts "Creating a community..."
 my_community = Community.create(address: "Dircksenstraße 117, 10178 Berlin")
+
+puts "Creating profiles..."
 ana_profile = Profile.create(floor: "VH, 1.OG", phone_number: "123456789123", about: "Hi, this is a first test")
 seb_profile = Profile.create(floor: "HH, 1.EG", phone_number: "123456789123", about: "Hi, this is a second test")
+
+puts "Creating users..."
 ana = User.new(email: "ana@garcia.com", password: "123456", first_name: "Ana", last_name: "Garcia")
 ana.profile = ana_profile
 ana.community = my_community
@@ -18,6 +24,8 @@ seb.profile = seb_profile
 seb.community = my_community
 seb.save
 ana.save
+
+puts "Creating cards..."
 Card.create(board: "community", category: "action", title: "first card", description: "just 1 test", start_date: "11Febr", start_time: "15pm", user: ana)
 Card.create(board: "parcels", category: "request", title: "second card", description: "just 2 test", start_date: "12Febr", start_time: "15pm", user: seb)
 Card.create(board: "missed", category: "info", title: "third card", description: "just 3 test", start_date: "13Febr", start_time: "15pm", user: ana)

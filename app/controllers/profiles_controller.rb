9424ea717aa4f0804a35601444127f61a1ad36a6
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-03-02 14:28:04 +0100

    added controller actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,23 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: :update
 
+  def new
+    @profile = Profile.new
+    @user = current_user
+  end
+
+  def create
+    @profile = Profile.new(set_params)
+    @user = current_user
+    @profile.user = @user
+    if @profile.save
+      # redirect_to "cards/"
+      redirect_to root
+    else
+      render :new
+    end
+  end
+
   def update
     @profile.update(set_params)
     redirect_to "cards/"

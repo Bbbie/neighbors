8095136a8bc263d612453b37b01655692d5a7199
Debbie <sepp.der.bauer@alpenjodel.de>
2021-03-02 18:33:52 +0100

    create collections to show in cards index, all exept from missed

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,16 +2,26 @@ class CardsController < ApplicationController
   before_action :set_card, only: [:show, :edit]
 
   def index
-    # All cards
-    @cards = policy_scope(Card)
-    # 4 Tabs in nav bar
-    # @missed_cards = @cards.where()
-    @parcels_cards = @cards.where(board: "parcels")
-    @mutual_help_cards = @cards.where(board: "mutual_help")
-    @community_cards = @cards.where(board: "community")
-    # My cards (with one filter: (archived: true)
-    @my_cards = @cards.where(:user == current_user)
-    @my_archived_cards = @my_cards.where(archived: true)
+    if params[:query] == "parcels"
+      @cards = policy_scope(Card).where(board: "parcels")
+      @title = "Parcels"
+    elsif params[:query] == "mutual-help"
+      @cards = policy_scope(Card).where(board: "mutual_help")
+      @title = "Mutual help"
+    elsif params[:query] == "community"
+      @cards = policy_Timescope(Card).where(board: "community")
+      @title = "Community"
+    # elsif params[:query] == "missed"
+    #   @cards = policy_scope(Card).where(created_at.to_i > currentuser.last_sign_in_at.to_i)
+    elsif params[:query] == "my-cards-archived"
+      @cards = policy_scope(Card).where(user: current_user, archived: true)
+      @title = "My archived cards"
+      # @link = link_to "All of my cards", cards_path(query: "my-cards-archived")
+    elsif params[:query] == "my-cards"
+      @cards = policy_scope(Card).where(user: current_user)
+      @title = "My cards"
+      # @link = link_to "All of my cards", cards_path(query: "my-cards")
+    end
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,5 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 end

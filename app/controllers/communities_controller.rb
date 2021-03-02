9ab42110a5ed7c10df6d5a97abfad5cacddd43cd
Davinia Tosco Abreu <daviniatosco@gmail.com>
2021-03-02 11:35:03 +0000

    create index action in communities controller

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,2 +1,11 @@
 class CommunitiesController < ApplicationController
+  def index
+    @communities = Community.all
+    @markers = @communities.geocoded.map do |community|
+      {
+        lat: community.latitude,
+        lng: community.longitude
+      }
+    end
+  end
 end

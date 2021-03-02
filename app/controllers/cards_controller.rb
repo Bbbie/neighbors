81b3a83e9bc59f88cf1eafb5ebb86a5cbfee6a74
Debbie <sepp.der.bauer@alpenjodel.de>
2021-03-02 20:01:33 +0100

    add links to nav bar

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,25 +2,39 @@ class CardsController < ApplicationController
   before_action :set_card, only: [:show, :edit]
 
   def index
+    # View parcels board
     if params[:query] == "parcels"
       @cards = policy_scope(Card).where(board: "parcels")
       @title = "Parcels"
+
+    # View mutual help board
     elsif params[:query] == "mutual-help"
       @cards = policy_scope(Card).where(board: "mutual_help")
       @title = "Mutual help"
+
+    # View community board
     elsif params[:query] == "community"
-      @cards = policy_Timescope(Card).where(board: "community")
+      @cards = policy_scope(Card).where(board: "community")
       @title = "Community"
+
+    # View missed board
     # elsif params[:query] == "missed"
     #   @cards = policy_scope(Card).where(created_at.to_i > currentuser.last_sign_in_at.to_i)
-    elsif params[:query] == "my-cards-archived"
-      @cards = policy_scope(Card).where(user: current_user, archived: true)
-      @title = "My archived cards"
-      # @link = link_to "All of my cards", cards_path(query: "my-cards-archived")
+
+    # View my cards page
     elsif params[:query] == "my-cards"
       @cards = policy_scope(Card).where(user: current_user)
       @title = "My cards"
-      # @link = link_to "All of my cards", cards_path(query: "my-cards")
+
+    # View
+    elsif params[:query] == "my-cards-archived"
+      @cards = policy_scope(Card).where(user: current_user, archived: true)
+      @title = "My archived cards"
+
+    # Default to board = missed
+    else
+      @cards = policy_scope(Card).where(board: "parcels")
+      @title = "Parcels"
     end
   end
 

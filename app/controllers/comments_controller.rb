94b9a94220c4a2797f8388d414927aa4ac13401d
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-03-04 15:39:59 +0100

    fixing bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    raise
     @comment = Comment.new(comment_params)
     @comment.user = current_user
     @comment.card = Card.find(params[:card_id])
     @comment.save
     authorize @comment
-    redirect_to cards_path
   end
 
   private

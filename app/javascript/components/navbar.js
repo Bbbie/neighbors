cc3b1ec9b659a2c9a428c95992e8ac4295ce6eee
Debbie <sepp.der.bauer@alpenjodel.de>
2021-03-06 21:00:52 +0100

    tried to change nav on home on scroll but still need to fix the look, when it switches and how to correctly do the opposite when scrolling back to top without bugs

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,24 @@
+const navbarScroll = () => {
+  const navbar = document.querySelector("#nav");
+  const logo = document.querySelector(".nav-logo-long")
+  const button = document.querySelector(".btn-login")
+
+  window.addEventListener('scroll', (event) => {
+    // console.log('they see me scrollin');
+    if (window.scrollY >= 30) {
+      navbar.classList.remove('nav-transparent');
+      navbar.classList.add('nav-grey');
+      logo.classList.add('nav-logo-grey');
+      button.classList.remove('btn-secondary-white');
+      button.classList.add('btn-secondary');
+    }
+    // else
+    //   navbar.classList.add('nav-transparent');
+    //   navbar.classList.remove('nav-grey');
+    //   logo.classList.remove('nav-logo-grey');
+    //   button.classList.add('btn-secondary-white');
+    //   button.classList.remove('btn-secondary');
+  })
+};
+
+export { navbarScroll };

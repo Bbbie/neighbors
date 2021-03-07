e03612ffa465e58b6bbd41016aa46b6e64a3d432
Debbie <sepp.der.bauer@alpenjodel.de>
2021-03-07 15:52:19 +0100

    tried to fix the display of start/end dates & start/end times. One more bug, then all good

--- app/policies/card_policy.rb
+++ app/policies/card_policy.rb
@@ -2,6 +2,9 @@ class CardPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope.all.order(created_at: :desc)
+      # scope.where(current_user == user).order(created_at: :desc).or(scope.where(record.users.include?(user))).order(created_at: :desc)
+      # scope.where(current_user == user).order(created_at: :desc) || scope.where(record.users.exists?(user)).order(created_at: :desc)
+      # scope.where(current_user == user).order(created_at: :desc) || scope.where(record.users.present?(user)).order(created_at: :desc)
     end
   end
 

c910bb99539be0b4253f3895c9746dd95b50f0a8
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-03-09 12:40:09 +0100

    now all buttons where default is used working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,22 @@
 module ApplicationHelper
-  def avatar_for(profile)
+  def navbar_avatar_for(profile)
     default_avatars = ['empty-avatar_1.svg', 'empty-avatar_2.svg', 'empty-avatar_3.svg', 'empty-avatar_4.svg']
     @avatar = profile.avatar
     @avatar_user = image_tag(default_avatars.sample, class: "avatar-big dropdown-toggle", id: "navbarDropdown", data: { toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false)
+    return @avatar_user
+  end
+
+  def index_avatar_for(profile)
+    default_avatars = ['empty-avatar_1.svg', 'empty-avatar_2.svg', 'empty-avatar_3.svg', 'empty-avatar_4.svg']
+    @avatar = profile.avatar
+    @avatar_user = image_tag(default_avatars.sample, class: "avatar-big")
+    return @avatar_user
+  end
+
+  def list_avatar_for(profile)
+    default_avatars = ['empty-avatar_1.svg', 'empty-avatar_2.svg', 'empty-avatar_3.svg', 'empty-avatar_4.svg']
+    @avatar = profile.avatar
+    @avatar_user = image_tag(default_avatars.sample, class: "avatar-small-list")
     return @avatar_user
   end
 end

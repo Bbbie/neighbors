c3666c9b07e4ee76cdf39f4af5789f0b0187692d
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-03-09 13:00:35 +0100

    almost finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,30 @@
 module ApplicationHelper
-  def navbar_avatar_for(profile)
+  def choose_avatar
     default_avatars = ['empty-avatar_1.svg', 'empty-avatar_2.svg', 'empty-avatar_3.svg', 'empty-avatar_4.svg']
+    default_avatars.sample
+  end
+
+  def navbar_avatar_for(profile)
     @avatar = profile.avatar
-    @avatar_user = image_tag(default_avatars.sample, class: "avatar-big dropdown-toggle", id: "navbarDropdown", data: { toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false)
+    @avatar_user = image_tag(choose_avatar, class: "avatar-big dropdown-toggle", id: "navbarDropdown", data: { toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false)
     return @avatar_user
   end
 
   def index_avatar_for(profile)
-    default_avatars = ['empty-avatar_1.svg', 'empty-avatar_2.svg', 'empty-avatar_3.svg', 'empty-avatar_4.svg']
     @avatar = profile.avatar
-    @avatar_user = image_tag(default_avatars.sample, class: "avatar-big")
+    @avatar_user = image_tag(choose_avatar, class: "avatar-big")
     return @avatar_user
   end
 
   def list_avatar_for(profile)
-    default_avatars = ['empty-avatar_1.svg', 'empty-avatar_2.svg', 'empty-avatar_3.svg', 'empty-avatar_4.svg']
     @avatar = profile.avatar
-    @avatar_user = image_tag(default_avatars.sample, class: "avatar-small-list")
+    @avatar_user = image_tag(choose_avatar, class: "avatar-small-list")
+    return @avatar_user
+  end
+
+  def profile_avatar_for(profile)
+    @avatar = profile.avatar
+    @avatar_user = image_tag(choose_avatar, class: "profile-picture")
     return @avatar_user
   end
 end

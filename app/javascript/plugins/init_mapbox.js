7b29dd328d91ce00a1986b27fd9e3d10d060245f
Debbie <sepp.der.bauer@alpenjodel.de>
2021-03-10 16:08:56 +0100

    swapped map style

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -15,7 +15,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/ammyvare/ckll1vrvz0h8g17ppqurhprd0'
+      style: 'mapbox://styles/bbbbbie/ckll22xlv10w517qw4sm7fcy9'
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);

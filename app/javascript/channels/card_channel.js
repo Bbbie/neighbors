f854d653add8615f1862974103855be4b86633c2
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-03-11 17:51:50 +0100

    scrolled when click on card added

--- app/javascript/channels/card_channel.js
+++ app/javascript/channels/card_channel.js
@@ -7,12 +7,17 @@ const initCardCable = () => {
       const id = chat.dataset.cardId;
       const messagesContainer = document.getElementById(`messages-${id}`);
       const formField = document.querySelector(`.input-${id}`);
+      const card = document.querySelector(`#button-card-${id}`);
+
+      card.addEventListener('click', (event) => {
+        gotoBottom(id);
+      })
+
       if (messagesContainer) {
 
         consumer.subscriptions.create({ channel: "CardChannel", id: id }, {
           received(data) {
             messagesContainer.insertAdjacentHTML('beforeend', data);
-            // clearForm();
             formField.querySelector('#comment_content').value = '';
             gotoBottom(id);
           },
@@ -24,19 +29,9 @@ const initCardCable = () => {
 }
 
 const gotoBottom = (id) => {
-   const element = document.getElementById(`messages-${id}`);
-   element.scrollTop = element.scrollHeight - element.clientHeight;
+  const element = document.getElementById(`messages-${id}`);
+  element.scrollTop = element.scrollHeight - element.clientHeight;
 }
 
-// var objDiv = document.getElementById("your_div");
-// objDiv.scrollTop = objDiv.scrollHeight;
-
-// const clearForm = () => {
-//   const forms = document.querySelectorAll('.new_comment');
-//   forms.forEach((form) => {
-//         form.reset();
-//       })
-// }
 
 export { initCardCable };
-// export { clearForm };

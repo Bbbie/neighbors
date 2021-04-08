ca8476279bab1d318207da1ea37722aa163415d8
Debbie <sepp.der.bauer@alpenjodel.de>
2021-04-08 12:17:07 +0200

    try implementing closing neighbors index popup when clicking outside it. Does not work yet.

--- app/javascript/components/neighbors-index.js
+++ app/javascript/components/neighbors-index.js
@@ -4,15 +4,29 @@ const neighbors = () => {
   const close = document.querySelector("#ic-close")
   const list = document.querySelector("#neighbors-index");
 
-  if (avatars) {
-  avatars.addEventListener("click", (event) => {
+  const toggleClass = () => {
     list.classList.toggle("display-none");
-  })};
+  }
+
+  const clickOutside = (event) => {
+    const target = event.currentTarget;
+    const visible = list && !list.classList.contains('display-none');
+
+    if ( visible && target !== list ) {
+      toggleClass();
+    }
+  }
+
+ document.addEventListener("click", clickOutside(event));
+
+  if (avatars) {
+    avatars.addEventListener("click", toggleClass);
+  }
 
   if (close) {
-    close.addEventListener("click", (event) => {
-      list.classList.toggle("display-none");
-    })};
+    close.addEventListener("click", toggleClass);
+  }
+
 };
 
 export { neighbors };

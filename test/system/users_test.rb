8334ae779a0a7ad6263873f96604ba07d2ddc65e
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-19 19:39:41 +0200

    added preliminary code for users testing

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -2,6 +2,16 @@ require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
   test "let's sign up" do
-
+    visit root_path
+    click_on("Sign up")
+    fill_in "form-control", with: "Dircksenstraße 117, 10178 Berlin"
+    click_on "Find community"
+    click_on "Create account"
+    fill_in "user_first_name", with: "George"
+    fill_in "user_last_name", with: "Example"
+    fill_in "user_email", with: "george@example.com"
+    # fill_in "user_first_name", with: "George"
+    click_on "Create account"
+    save_and_open_screenshot
   end
 end

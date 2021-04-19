7bd9ecf0990302ab60d484ac613de2c09ba990f3
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-19 18:19:23 +0200

    first test for creating profile added

--- test/system/profiles_test.rb
+++ test/system/profiles_test.rb
@@ -1,12 +1,12 @@
 require "application_system_test_case"
 
 class ProfilesTest < ApplicationSystemTestCase
-  test "visiting a profile" do
-    log_in as users(:ammy)
+  test "create new profile after log in, redirect to cards#index" do
+    login_as users(:ammy)
     visit "/profiles/new"
-    fill_in "floor", with: "VH 1 EG"
-    fill_in "phone_number", with: "12345423"
-    fill_in "about", with: "Hey there I am a test"
+    fill_in "profile_floor", with: "VH 1 EG"
+    fill_in "profile_phone_number", with: "12345423"
+    fill_in "profile_about", with: "Hey there I am a test"
     click_on "Save"
 
     save_and_open_screenshot

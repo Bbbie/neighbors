072a13c7497bb7b28b773822e76c112b181e31f5
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-19 19:46:08 +0200

    improved user test code

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -3,14 +3,15 @@ require "application_system_test_case"
 class UsersTest < ApplicationSystemTestCase
   test "let's sign up" do
     visit root_path
-    click_on("Sign up")
-    fill_in "form-control", with: "Dircksenstraße 117, 10178 Berlin"
+    click_on("Sign Up")
+    fill_in "address_input", with: "Dircksenstraße 117, 10178 Berlin"
     click_on "Find community"
     click_on "Create account"
     fill_in "user_first_name", with: "George"
     fill_in "user_last_name", with: "Example"
     fill_in "user_email", with: "george@example.com"
-    # fill_in "user_first_name", with: "George"
+    fill_in "user_password", with: "123456"
+    fill_in "user_password_confirmation", with: "123456"
     click_on "Create account"
     save_and_open_screenshot
   end

e9bc0b51b32fa65e371f0391cffd7af6e09c688e
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-20 19:35:10 +0200

    beginning of test

--- test/system/cards_test.rb
+++ test/system/cards_test.rb
@@ -2,5 +2,27 @@ require "application_system_test_case"
 
 class CardsTest < ApplicationSystemTestCase
   test "login and create card" do
+    # TO HAVE A COMMUNITY LINKED TO USER
+    visit root_path
+    click_on("Sign Up")
+
+    fill_in "address_input", with: "Dircksenstraße 117, 10178 Berlin"
+    click_on "Find community"
+    click_on "Create account"
+
+    fill_in "user_first_name", with: "George"
+    fill_in "user_last_name", with: "Example"
+    fill_in "user_email", with: "george@example.com"
+    fill_in "user_password", with: "123456"
+    fill_in "user_password_confirmation", with: "123456"
+    click_on "Create account"
+
+    fill_in "profile_floor", with: "VH 1 EG"
+    fill_in "profile_phone_number", with: "12345423"
+    fill_in "profile_about", with: "Hey there I am a test"
+    click_on "Save"
+
+    visit cards_path
+    save_and_open_screenshot
   end
 end
